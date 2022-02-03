9caefd618b578f95e719f3ec39b3d1922ac38591
Victor Hou <hou_v@yahoo.com>
2022-02-03 09:50:07 -0800

    fixed seeder issue

--- app/seeds/favorite_foods.py
+++ app/seeds/favorite_foods.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def seed_favorite_foods():
     food1 = Favorite_Foods(name='eggs and rice', calories=400, carbohydrates=36, fat=12, protein=24, user_id=1, created_at=datetime.now())
     food2 = Favorite_Foods(name='soup', calories=160, carbohydrates=3, fat=6, protein=12, user_id=1, created_at=datetime.now())
-    food3 = Favorite_Foods(name='steak', calories=330, carbohydrates=0, fat=3, protein=70, user_id=1, created_at=datetime.now())
+    food3 = Favorite_Foods(name='steak', calories=400, carbohydrates=0, fat=3, protein=70, user_id=1, created_at=datetime.now())
     food4 = Favorite_Foods(name='chicken and waffles', calories=560, carbohydrates=50, fat=13, protein=50, user_id=1, created_at=datetime.now())
 
     db.session.add(food1)
